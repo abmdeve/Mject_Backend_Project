a9a5dbf87b9f2e2d887ae7d54f4367c867f879e5
abmdeve <tundederome@gmail.com>
2023-11-19 15:40:53 +0100

    Start of the project
    Setting up the tables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FormationModule } from './formation/formation.module';
+import { RoleModule } from './role/role.module';
+import { StatusModule } from './status/status.module';
+import { UsersModule } from './users/users.module';
+import { NewsModule } from './news/news.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [],
+  imports: [FormationModule, RoleModule, StatusModule, UsersModule, NewsModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
